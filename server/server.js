05401ca926038ddfaef6fe05a94f596beb0a4d0e
Rachel Park <vinetree@gmail.com>
2019-08-28 14:42:22 -0400

    rachel-backend elapsed time feature

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ wss.on("connection", function connection(ws) {
   app.use("*", (req, res, next) => {
     //Obtain the request time
     req.requestTime = Date.now();
-    
+    const requestTimeCalc = process.hrtime();
     //Data we're going to send to the establish WebSocket Server for the Front-end to grab;
     //All set as null until we define the data coming in.
     const data = {
@@ -41,7 +41,9 @@ wss.on("connection", function connection(ws) {
     //Sending data object to WebSocket Server for Front-End to grab.
     res.on('finish', function (err, dataParam) {
       !res.statusCode ? data.statusCode = 'There is no status code' : data.statusCode = res.statusCode;
-      data.elapsedTime = Date.now() - data.requestTime;
+      //we would like to get the elasedTime in milliseconds(precise!)
+      const elapsedTime = process.hrtime(requestTimeCalc);
+      data.elapsedTime = (elapsedTime[0]/1000) + (elapsedTime[1]/1000000);
       ws.send(JSON.stringify(data));
     });
     //Connection Confirmation.
