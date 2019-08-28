a975ed1df103c7c3f3e56fcc0ff24300ff5f151f
David Neuhuas <david@neuha.us>
2019-08-28 16:23:47 -0400

    started reworking front end, added manifest.json for chrome extension proof of concept

--- client/index.js
+++ client/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store.js";
+// import store from "./store.js";
+// import persistor from "./store.js";
+import configureStore from "./store.js";
+const {persistor, store} = configureStore();
 import App from "./App.jsx";
 import './styles.scss'
+import { PersistGate } from 'redux-persist/integration/react'
+// import { strict } from "assert";
 
 //Render for main App Component, that has store inside as an attibute inside it's Provider;
 //Provides store to component;
 ReactDOM.render(
+  
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
