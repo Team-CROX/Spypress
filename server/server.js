eeaccfdc8d696ef90364f0544d0ae536ddf84c01
Sam Parsons <samparsons269@gmail.com>
2019-08-28 17:10:57 -0400

    cpu usage, update significant digits for timing

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const bodyParser = require('body-parser');
 //Websocket Connection to port 2000;
 const wss = new WebSocket.Server({ port: 2000 });
 
+// [seconds, nanoseconds] => milliseconds
+function secNSec2ms (secNSec) {
+  return secNSec[0] * 1000 + secNSec[1] / 1000000;
+}
+
+// microseconds => milliseconds
+function MSec2ms(MSec) {
+  return MSec / 1000;
+}
+
 //Event listener for WebSocket Connection.
 wss.on("connection", function connection(ws) { 
   
@@ -16,9 +26,16 @@ wss.on("connection", function connection(ws) {
 
   //Listen on all Requests
   app.use("*", (req, res, next) => {
-    //Obtain the request time
+
+    //Obtain the request time in milliseconds
     req.requestTime = Date.now();
+
+    // time of request in [seconds, nanoseconds] 
     const requestTimeCalc = process.hrtime();
+
+    // get start CPU time - microseconds
+    const startUsage = process.cpuUsage();
+
     //Data we're going to send to the establish WebSocket Server for the Front-end to grab;
     //All set as null until we define the data coming in.
     const data = {
@@ -29,6 +46,8 @@ wss.on("connection", function connection(ws) {
       statusCode: null, 
       requestTime: null,
       elapsedTime: null,
+      systemUsage: null,
+      applicationUsage: null
     };
 
     //Defining any requests that comes in such as Methods, Headers, Cookies & Body.
@@ -41,9 +60,26 @@ wss.on("connection", function connection(ws) {
     //Sending data object to WebSocket Server for Front-End to grab.
     res.on('finish', function (err, dataParam) {
       !res.statusCode ? data.statusCode = 'There is no status code' : data.statusCode = res.statusCode;
+      
       //we would like to get the elasedTime in milliseconds(precise!)
       const elapsedTime = process.hrtime(requestTimeCalc);
-      data.elapsedTime = (elapsedTime[0]/1000) + (elapsedTime[1]/1000000);
+      const elapsedTimeMS = secNSec2ms(elapsedTime);
+
+      // elapsed time between request received and response sent
+      data.elapsedTime = Number(secNSec2ms(elapsedTime).toFixed(3));
+
+      // diff in CPU usage in microseconds
+      const elapUsage = process.cpuUsage(startUsage);
+
+      // application's usage
+      const elapUser = MSec2ms(elapUsage.user);
+      // system's usage
+      const elapSystem = MSec2ms(elapUsage.system);
+
+      // percentages of usage by system and application
+      data.systemUsage = Number((100 * (elapSystem / elapsedTimeMS)).toFixed(3));
+      data.applicationUsage = Number((100 * (elapUser / elapsedTimeMS)).toFixed(3));
+
       ws.send(JSON.stringify(data));
     });
     //Connection Confirmation.
