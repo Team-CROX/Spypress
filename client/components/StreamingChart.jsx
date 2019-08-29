5ea21b2122a6f0cfb46ae4c11ad7c440c0bbb291
David Neuhuas <david@neuha.us>
2019-08-29 12:41:56 -0400

    ChartBeat.js is born

--- client/components/StreamingChart.jsx
+++ client/components/StreamingChart.jsx
@@ -1,45 +1,86 @@
 import React, { Component } from "react";
 import 'chartjs-plugin-streaming';
-import { Line } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 import { Bubble } from 'react-chartjs-2';
 // import 'chartjs-plugin-streaming';
 
 class StreamingChart extends Component {
+  constructor(props) {
+    super(props)
+    this.messages = this.messages.bind(this);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    return false;
+  }
 
-render(){
-  return (
-    
-    <Bubble
-    data={{
-      datasets: [{
-        label: 'demo',
-        backgroundColor: 'rgba(75,192,192,1)',
-        data: []
-      }]
-    }}
-    options={{
-      plugins: {
-        streaming: {
-          onRefresh: function(chart) {
-            chart.data.datasets[0].data.push({
-              x: Date.now(),
-              y: Math.random() * 100,
-            });
+  messages() {return this.props.messageArr}
+  render(){
+    return (
+      
+      <Line
+        data={{
+          datasets: [
+            {
+              label: 'systemUsage',
+              borderColor: '#9f5a70',
+              fill: false,
+              data: []
+            },
+            {
+              label: 'applicationUsage',
+              borderColor: '#398eba',
+              fill: false,
+              data: []
+            },
+            {
+              label: 'elapsedTime',
+              borderColor: '#679d70',
+              fill: false,
+              data: []
+            },
+          ]
+        }}
+        options={{
+          plugins: {
+            streaming: {
+              onRefresh: function(chart) {
+                let messageArr = JSON.parse(JSON.parse(globalThis.localStorage["persist:root"]).messageArray);
+                // datasets[0] is  systemUsage
+                chart.data.datasets[0].data.push({
+                  x: Date.now(),
+                  y: messageArr[messageArr.length - 1].systemUsage,
+                });
+                // datasets[1] is  applicationUsage
+                chart.data.datasets[1].data.push({
+                  x: Date.now(),
+                  y: messageArr[messageArr.length - 1].applicationUsage,
+                });
+                // datasets[2] is  elapsedTime
+                chart.data.datasets[2].data.push({
+                  x: Date.now(),
+                  y: messageArr[messageArr.length - 1].elapsedTime,
+                });
+              },
+              delay: 500,
+              refresh: 500,
+              frameRate: 30,
+              duration: 900000 // 3600000 = 1hour
+            }
           },
-          delay: 500,
-          refresh: 500,
-          frameRate: 30,
-          duration: 900000 // 3600000 = 1hour
-        }
-      },
-      scales: {
-        xAxes: [{
-          type: 'realtime'
-        }]
-      }
-    }}
-    />
+          scales: {
+            xAxes: [{
+              type: 'realtime',
+              realtime: {
+                duration: 20000,
+                refresh: 1000,
+                delay: 2000,
+                // onRefresh: onRefresh
+              }
+            }]
+          }
+        }}
+      />
     )
   }
 }
